b7a77f37419d120917e4658a06e1852e1fbdcb56
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-06-26 10:46:05 +0200

    setup repo structure using copier template

---
+++ src/ghg_forcing_for_cmip_comparison/__init__.py
@@ -0,0 +1,7 @@
+"""
+Intercomparison of CMIP7 data with earth observations as part of GHG forcing for CMIP project.
+"""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("ghg_forcing_for_cmip_comparison")
