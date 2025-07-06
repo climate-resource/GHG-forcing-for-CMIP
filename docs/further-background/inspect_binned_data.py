f26940455e76a93db610a01363cdf99d86ebc3bf
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-07-06 14:58:09 +0200

    download datasets for docs outside of notebooks

--- docs/further-background/inspect_binned_data.py
+++ docs/further-background/inspect_binned_data.py
@@ -15,39 +15,19 @@
 # %%
 import numpy as np
 import pandas as pd
-import requests
-import zipfile
-import io
 
 from ghg_forcing_for_cmip_comparison import plotting
+from ghg_forcing_for_cmip_comparison.utils import load_data
 
 # %% [markdown]
 # # Inspect binned data
-
-# %%
-url_ch4 = "https://github.com/climate-resource/GHG-forcing-for-CMIP-comparison/releases/download/v0.1.0-alpha/ch4.zip"
-url_co2 = "https://github.com/climate-resource/GHG-forcing-for-CMIP-comparison/releases/download/v0.1.0-alpha/co2.zip"
-
-dfs_ch4, dfs_co2 = ({}, {})
-
-for url, dfs in zip([url_ch4, url_co2], [dfs_ch4, dfs_co2]):
-    response = requests.get(url)
-
-    # Open the zip file in memory
-    with zipfile.ZipFile(io.BytesIO(response.content)) as thezip:
-        # Loop through each file inside the zip
-        for filename in thezip.namelist():
-            if filename.endswith('binned.csv'):
-                with thezip.open(filename) as file:
-                    # Read CSV into pandas dataframe
-                    dfs[filename] = pd.read_csv(file)
 
 # %% [markdown]
 # ## Methane ($CH_4$)
 
 # %%
 # load data set
-d_binned = dfs_ch4["ch4_binned.csv"]
+d_binned = load_data("ch4_binned.csv")
 
 d_binned.head()
 
@@ -69,7 +49,7 @@ plotting.plot_map_grid(d_binned)
 
 # %%
 # load data set
-d_binned = dfs_co2["co2_binned.csv"]
+d_binned = load_data("co2_binned.csv")
 
 d_binned.head()
 
--- docs/further-background/inspect_interpolated_data.py
+++ docs/further-background/inspect_interpolated_data.py
@@ -15,40 +15,20 @@
 # %%
 import numpy as np
 import pandas as pd
-import requests
-import zipfile
-import io
 
 from ghg_forcing_for_cmip_comparison import plotting
+from ghg_forcing_for_cmip_comparison.utils import load_data
 
 # %% [markdown]
 # # Inspect interpolated data
-
-# %%
-url_ch4 = "https://github.com/climate-resource/GHG-forcing-for-CMIP-comparison/releases/download/v0.1.0-alpha/ch4.zip"
-url_co2 = "https://github.com/climate-resource/GHG-forcing-for-CMIP-comparison/releases/download/v0.1.0-alpha/co2.zip"
-
-dfs_ch4, dfs_co2 = ({}, {})
-
-for url, dfs in zip([url_ch4, url_co2], [dfs_ch4, dfs_co2]):
-    response = requests.get(url)
-
-    # Open the zip file in memory
-    with zipfile.ZipFile(io.BytesIO(response.content)) as thezip:
-        # Loop through each file inside the zip
-        for filename in thezip.namelist():
-            if filename.endswith(('binned.csv', 'interpolated.csv')):
-                with thezip.open(filename) as file:
-                    # Read CSV into pandas dataframe
-                    dfs[filename] = pd.read_csv(file)
 
 # %% [markdown]
 # ## Methane ($CH_4$)
 
 # %%
 # load data sets
-d_interpol = dfs_ch4["ch4_interpolated.csv"]
-d_binned = dfs_ch4["ch4_binned.csv"]
+d_interpol = load_data("ch4_interpolated.csv")
+d_binned = load_data("ch4_binned.csv")
 
 d_interpol.head()
 
@@ -84,8 +64,8 @@ plotting.plot_annual_concentration(
 
 # %%
 # load data sets
-d_interpol = dfs_co2["co2_interpolated.csv"]
-d_binned = dfs_co2["co2_binned.csv"]
+d_interpol = load_data("co2_interpolated.csv")
+d_binned = load_data("co2_binned.csv")
 
 d_interpol.head()
 
