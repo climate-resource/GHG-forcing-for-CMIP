76e295783439b586f942e5a21e686c6901041646
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-07-06 20:14:43 +0200

    update copier

---
+++ docs/further-background/inspect_binned_data.py
@@ -0,0 +1,52 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.6
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+#%%
+import numpy as np
+
+from ghg_forcing_for_cmip import plotting
+from .utils import load_data
+#%% [markdown]
+# # Inspect binned data
+#%% md
+# ## Methane ($CH_4$)
+#%%
+# load data set
+d_binned = load_data("ch4_binned.csv")
+
+d_binned.head()
+#%% [markdown]
+# ### Inspect gridding
+# + data are required to be on a $5^\circ \times 5^\circ$ grid
+#%%
+print("latitude:")
+print(np.sort(d_binned.lat.unique()))
+print("longitude:")
+print(np.sort(d_binned.lon.unique()))
+#%%
+plotting.plot_map_grid(d_binned)
+#%% [markdown]
+# ## Carbon Dioxide ($CO_2$)
+#%%
+# load data set
+d_binned = load_data("co2_binned.csv")
+
+d_binned.head()
+#%%
+print("latitude:")
+print(np.sort(d_binned.lat.unique()))
+print("longitude:")
+print(np.sort(d_binned.lon.unique()))
+#%%
+plotting.plot_map_grid(d_binned)
