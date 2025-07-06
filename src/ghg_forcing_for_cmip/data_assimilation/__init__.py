51db57e7a4b1d702cac3ed1425b7f6fe120a0a2d
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-07-06 22:07:48 +0200

    add python package

--- src/ghg_forcing_for_cmip/data_assimilation/__init__.py
+++ src/ghg_forcing_for_cmip/data_assimilation/__init__.py
@@ -3,3 +3,7 @@ Data assimilation
 
 Compare CMIP7 data with earth observations as part of GHG forcing for CMIP project.
 """
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("ghg_forcing_for_cmip.data_assimilation")
