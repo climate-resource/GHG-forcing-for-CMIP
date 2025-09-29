e150fa025ea03a9bd6f6e32e5422da10ab62f863
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-09-29 10:38:59 +0200

    add test for clean_and_save func

--- tests/unit/test_download_data.py
+++ tests/unit/test_download_data.py
@@ -5,10 +5,14 @@ Unit tests for downloading GHG concentrations
 from web APIs
 """
 
+import os
+import shutil
+
 import numpy as np
 import pandas as pd
+import pytest
 
-from ghg_forcing_for_cmip.download_data import stats_from_events
+from ghg_forcing_for_cmip.download_data import clean_and_save, stats_from_events
 
 
 def test_stats_from_events():
@@ -26,3 +30,27 @@ def test_stats_from_events():
     np.testing.assert_almost_equal(observed_df.value.mean(), expected_mean, decimal=3)
     np.testing.assert_almost_equal(observed_df.std_dev.mean(), expected_std, decimal=3)
     np.testing.assert_equal(np.unique(observed_df.numb), expected_count)
+
+
+@pytest.mark.parametrize("gas", ["ch4", "co2"])
+def test_clean_and_save(gas):
+    # prepare test dataframe
+    test_df = pd.DataFrame()
+    for var, val in zip(
+        ["year", "month", "latitude", "longitude"], [2015, 1, 2.5, 2.5]
+    ):
+        test_df[var] = val
+    test_df["time"] = pd.to_datetime("01.02.2028")
+
+    # create folder if not existent
+    os.makedirs(f"tests/test-data/{gas}", exist_ok=True)
+
+    # run target function
+    clean_and_save(test_df, gas, "tests/test-data", False)
+
+    # check if files are saved
+    os.path.isfile(f"tests/test-data/{gas}/{gas}_raw.csv")
+    os.path.isfile(f"tests/test-data/{gas}/{gas}_raw.nc")
+
+    # remove test-folder after testing
+    shutil.rmtree(f"tests/test-data/{gas}")
