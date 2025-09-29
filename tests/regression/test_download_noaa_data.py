273e4585d84293415d81909bac5026cb73d435e6
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-09-29 17:10:13 +0200

    add merge_netCDFs to regression-test

--- tests/regression/test_download_noaa_data.py
+++ tests/regression/test_download_noaa_data.py
@@ -8,9 +8,14 @@ from web APIs
 import os
 
 import numpy as np
+import pandas as pd
 import pytest
 
-from ghg_forcing_for_cmip.download_data import download_zip_from_noaa, unzip_download
+from ghg_forcing_for_cmip.download_data import (
+    download_zip_from_noaa,
+    merge_netCDFs,
+    unzip_download,
+)
 
 
 @pytest.mark.parametrize("gas", ["co2", "ch4"])
@@ -29,6 +34,7 @@ def test_download_extract_noaa(gas, sampling_strategy):
 
     save_dir = "tests/test-data/"
 
+    # %% test downloading
     # download NOAA data in test directory
     download_zip_from_noaa(gas, sampling_strategy, save_to_path=save_dir)
 
@@ -41,6 +47,7 @@ def test_download_extract_noaa(gas, sampling_strategy):
     ), f"Expected file {expected_file} was not created"
     assert os.stat(expected_file).st_size > 0, "Downloaded file is empty"
 
+    # %% test unzipping of files
     # unzip folder
     unzip_download(zip_path=expected_file, extract_dir=save_dir + "extracted")
 
@@ -56,3 +63,41 @@ def test_download_extract_noaa(gas, sampling_strategy):
     assert len(all_files) == exp_numb_files
     # check that all observation sites are included as expected
     np.testing.assert_array_equal(np.sort(expected_sites), np.sort(observed_sites))
+
+    # %% test merging
+    df_all = []
+    df_all.append(
+        merge_netCDFs(
+            extract_dir=save_dir
+            + f"extracted/{gas}_surface-{sampling_strategy}_ccgg_netCDF"
+        )
+    )
+
+    df_combined = pd.concat(df_all)
+
+    expected_cols = [
+        "std_dev",
+        "numb",
+        "value",
+        "year",
+        "month",
+        "latitude",
+        "longitude",
+        "altitude",
+        "site_code",
+        "network",
+        "insitu_vs_flask",
+        "sampling_strategy",
+        "gas",
+        "unit",
+        "version",
+        "instrument",
+    ]
+
+    for col in expected_cols:
+        assert col in df_combined.columns, f"Can't find column {col} in data frame."
+
+    for code in expected_sites:
+        assert (
+            code.upper() in df_combined.site_code.unique()
+        ), f"Can't finde site_code {code.upper()} in data frame."
