2b39c8e868f915072d7ddd469badca4768539cfb
florence-bockting <florence.bockting@tu-dortmund.de>
2025-10-02 13:08:08 +0200

    refactoring: create download_ground_based module

--- src/ghg_forcing_for_cmip/download_ground_based.py
+++ src/ghg_forcing_for_cmip/download_ground_based.py
@@ -1,13 +1,11 @@
 """
-Download data
+Download ground-based data
 
 The task of this module is data scraping of the GHG
-concentration data from e.g. (A)GAGE, NOAA
+concentration data from (A)GAGE and NOAA networks
 """
 
 import os
-import shutil
-import zipfile
 from typing import Union
 
 import numpy as np
@@ -54,31 +52,6 @@ def download_zip_from_noaa(gas: str, sampling_strategy: str, save_to_path: str) 
         f.write(response.content)
 
     print(f"downloaded NOAA-zip ({gas}-{sampling_strategy}) to {save_to_path}")
-
-
-@task(description="Unzip downloaded data", cache_policy=CONFIG.CACHE_POLICIES)
-def unzip_download(zip_path: str, extract_dir: str) -> None:
-    """
-    Unzips a given ZIP file into the target directory.
-
-    Parameters
-    ----------
-    zip_path :
-        Path to the zip file (e.g., "data/downloads/noaa_ch4_surface_flask.zip")
-
-    extract_dir :
-        Path where the files should be extracted.
-    """
-    # make sure target directory exists
-    os.makedirs(extract_dir, exist_ok=True)
-
-    # unzip
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
-        zip_ref.extractall(extract_dir)
-
-    os.remove(zip_path)
-
-    print(f"Extracted {zip_path} to {extract_dir}")
 
 
 def stats_from_events(df: pd.DataFrame) -> pd.DataFrame:
@@ -365,55 +338,6 @@ def validate_surface_data(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-@task(
-    name="clean_and_save",
-    description="Clean directory and save file",
-    cache_policy=CONFIG.CACHE_POLICIES,
-)
-def clean_and_save(
-    df: pd.DataFrame, gas: str, save_to_path: str, remove_original_files: bool
-) -> None:
-    """
-    Clean up folder and save final dataset
-
-    Parameters
-    ----------
-    df :
-        final, post-processed data
-
-    gas :
-        greenhouse gas,
-        either ch4 or co2
-
-    save_to_path :
-        path to save the results
-
-    remove_original_files :
-        whether downloaded files should be kept;
-        otherwise they are removed
-    """
-    save_to_path = utils.ensure_trailing_slash(save_to_path)
-    # save as csv
-    df.to_csv(save_to_path + gas + f"/{gas}_raw.csv", index=False)
-
-    # save as netcdf
-    ds = xr.Dataset.from_dataframe(df)
-    year, month, latitude, longitude = (
-        df[var].unique() for var in ["year", "month", "latitude", "longitude"]
-    )
-    ds = ds.assign_coords(
-        {"year": year, "month": month, "latitude": latitude, "longitude": longitude}
-    )
-
-    # reconvert datetime format (info is lost when converting df to ds)
-    ds["time"] = df.time
-    ds.to_netcdf(save_to_path + gas + f"/{gas}_raw.nc")
-
-    # clean-up directory
-    if remove_original_files and os.path.exists(save_to_path + gas + "/original"):
-        shutil.rmtree(save_to_path + gas + "/original")
-
-
 @flow(name="download_surface_data", description="Download and preprocess surface data")
 def download_surface_data(
     gas: str, remove_original_files: bool, save_to_path: str = "data/downloads"
@@ -442,7 +366,7 @@ def download_surface_data(
             gas=gas, sampling_strategy=sampling, save_to_path=save_to_path
         )
 
-        unzip_download.with_options(name=f"unzip_download_{gas}_{sampling}")(
+        utils.unzip_download.with_options(name=f"unzip_download_{gas}_{sampling}")(
             zip_path=save_to_path + f"/noaa_{gas}_surface_{sampling}.zip",
             extract_dir=save_to_path + f"/{gas}/original",
         )
@@ -463,13 +387,14 @@ def download_surface_data(
     df_final = validate_surface_data(df_processed)
 
     # clean up repo and save file
-    clean_and_save(
+    utils.clean_and_save(
         df_final,
         gas=gas,
         save_to_path=save_to_path,
+        measurement_type="gb",
         remove_original_files=remove_original_files,
     )
 
 
 if __name__ == "__main__":
-    download_surface_data(gas="ch4", remove_original_files=True)
+    download_surface_data(gas="co2", remove_original_files=True)
