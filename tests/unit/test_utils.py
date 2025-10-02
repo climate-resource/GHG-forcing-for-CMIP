ce79da0080afa02bc97466170653d239da259635
florence-bockting <florence.bockting@tu-dortmund.de>
2025-10-02 13:06:19 +0200

    move test for save_and_clean to test_utils module

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -3,10 +3,12 @@ Test utils.py
 
 Unit tests for helper functions
 """
-
+import os
 import pytest
+import shutil
+import pandas as pd
 
-from ghg_forcing_for_cmip.utils import ensure_trailing_slash
+from ghg_forcing_for_cmip.utils import ensure_trailing_slash, clean_and_save
 
 
 @pytest.mark.parametrize(
@@ -25,3 +27,27 @@ def test_ensure_trailing_slash(test_path, expected):
         f"The observed test-path: {observed},"
         " does not match the expected path: {expected}"
     )
+
+
+@pytest.mark.parametrize("gas", ["ch4", "co2"])
+def test_clean_and_save(gas):
+    # prepare test dataframe
+    test_df = pd.DataFrame()
+    for var, val in zip(
+        ["year", "month", "latitude", "longitude"], [2015, 1, 2.5, 2.5]
+    ):
+        test_df[var] = val
+    test_df["time"] = pd.to_datetime("01.02.2028")
+
+    # create folder if not existent
+    os.makedirs(f"tests/test-data/{gas}", exist_ok=True)
+
+    # run target function
+    clean_and_save(test_df, gas, "tests/test-data", "gb", False)
+
+    # check if files are saved
+    os.path.isfile(f"tests/test-data/{gas}/{gas}_gb_raw.csv")
+    os.path.isfile(f"tests/test-data/{gas}/{gas}_gb_raw.nc")
+
+    # remove test-folder after testing
+    shutil.rmtree(f"tests/test-data/{gas}")
