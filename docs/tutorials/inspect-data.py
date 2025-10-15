257ef9c6f201efe98b03c3688e627456f6bebf4c
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-10-15 12:31:50 +0200

    update tutorial on collocated sites

--- docs/tutorials/inspect-data.py
+++ docs/tutorials/inspect-data.py
@@ -18,18 +18,20 @@
 
 # %%
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 from ghg_forcing_for_cmip import plotting
+from ghg_forcing_for_cmip.validation import compute_discrepancy_collocated
 
 # %%
-d_gb_ch4 = pd.read_csv("data/downloads/ch4/ch4_gb_raw.csv")
-d_gb_co2 = pd.read_csv("data/downloads/co2/co2_gb_raw.csv")
+d_gb_ch4 = pd.read_csv("../../data/downloads/ch4/ch4_gb_raw.csv")
+d_gb_co2 = pd.read_csv("../../data/downloads/co2/co2_gb_raw.csv")
 d_gb_ch4.head()
 
 # %%
-d_eo_ch4 = pd.read_csv("data/downloads/ch4/ch4_eo_raw.csv")
-d_eo_co2 = pd.read_csv("data/downloads/co2/co2_eo_raw.csv")
+d_eo_ch4 = pd.read_csv("../../data/downloads/ch4/ch4_eo_raw.csv")
+d_eo_co2 = pd.read_csv("../../data/downloads/co2/co2_eo_raw.csv")
 d_eo_ch4.head()
 
 # %% [markdown]
@@ -109,11 +111,15 @@ for i, year in enumerate([2018, 2020, 2022]):
 
 # %%
 # select collocated sites
-d_gb_ch4_sel = d_gb_ch4[["year", "month", "lat", "lon", "value"]].drop_duplicates()
+d_gb_ch4_sel = d_gb_ch4[
+    ["year", "month", "lat", "lon", "value", "site_code"]
+].drop_duplicates()
 d_eo_ch4_sel = d_eo_ch4[
     ["year", "month", "lat", "lon", "value", "pre", "vmr_profile_apriori"]
 ].drop_duplicates()
-d_gb_co2_sel = d_gb_co2[["year", "month", "lat", "lon", "value"]].drop_duplicates()
+d_gb_co2_sel = d_gb_co2[
+    ["year", "month", "lat", "lon", "value", "site_code"]
+].drop_duplicates()
 d_eo_co2_sel = d_eo_co2[
     ["year", "month", "lat", "lon", "value", "pre", "vmr_profile_apriori"]
 ].drop_duplicates()
@@ -130,8 +136,9 @@ d_colloc_co2 = d_gb_co2_sel.merge(
     how="inner",
     suffixes=["_gb", "_eo"],
 )
-d_colloc_ch4.to_csv("data/downloads/ch4_collocated.csv")
-d_colloc_co2.to_csv("data/downloads/co2_collocated.csv")
+
+d_colloc_ch4.to_csv("../../data/downloads/ch4_collocated.csv")
+d_colloc_co2.to_csv("../../data/downloads/co2_collocated.csv")
 d_colloc_ch4.head()
 
 # %%
@@ -141,13 +148,84 @@ plotting.plot_monthly_average(d_colloc_co2, "$CO_2$", axs[1])
 fig.suptitle("Annual, global average of GHG concentration")
 
 # %%
-plotting.plot_average_hemisphere(
-    d_colloc_ch4[d_colloc_ch4.value_gb > 1600].copy(),  # noqa: PLR2004
-    "$CH_4$",
+plotting.plot_average_hemisphere(d_colloc_ch4, "$CH_4$")
+
+# %%
+plotting.plot_average_hemisphere(d_colloc_co2, "$CO_2$")
+
+# %%
+d_col_sorted_co2 = (
+    d_colloc_co2.groupby(["lat", "lon", "site_code"])
+    .agg({"value_gb": "count"})
+    .reset_index()
+    .sort_values(by="value_gb", ascending=False)
+)
+d_col_sorted_ch4 = (
+    d_colloc_ch4.groupby(["lat", "lon", "site_code"])
+    .agg({"value_gb": "count"})
+    .reset_index()
+    .sort_values(by="value_gb", ascending=False)
+)
+d_col_sorted_ch4.rename(columns={"value_gb": "count"}).head(10)
+
+# %%
+sites_selected_co2 = ["SMO", "MLO", "TAP", "NMB"]
+sites_selected_ch4 = ["SMO", "MLO", "WLG", "ASK"]
+
+_, axs = plt.subplots(1, 2, constrained_layout=True, figsize=(8, 5))
+plotting.plot_map(
+    d_col_sorted_co2[d_col_sorted_co2.site_code.isin(sites_selected_co2)],
+    "Selected sites - CO2",
+    lon_value="lon",
+    lat_value="lat",
+    axs=axs[0],
+)
+plotting.plot_map(
+    d_col_sorted_ch4[d_col_sorted_ch4.site_code.isin(sites_selected_ch4)],
+    "Selected sites - CH4",
+    lon_value="lon",
+    lat_value="lat",
+    axs=axs[1],
 )
 
 # %%
-plotting.plot_average_hemisphere(
-    d_colloc_co2[d_colloc_co2.value_gb > 360].copy(),  # noqa: PLR2004
-    "$CO_2$",
-)
+d_col_sel_co2 = d_colloc_co2[
+    d_colloc_co2.site_code.isin(sites_selected_co2)
+].reset_index(drop=True)
+d_col_sel_ch4 = d_colloc_ch4[
+    d_colloc_ch4.site_code.isin(sites_selected_ch4)
+].reset_index(drop=True)
+
+d_col_sel_co2.to_csv("../../data/downloads/co2_collocated_sites.csv")
+d_col_sel_ch4.to_csv("../../data/downloads/ch4_collocated_sites.csv")
+
+# %%
+fig, axs = plt.subplots(2, 4, figsize=(11, 4), constrained_layout=True)
+for i, site in enumerate(sites_selected_co2):
+    d = d_col_sel_co2[d_col_sel_co2.site_code == site].copy()
+    rmse_co2 = compute_discrepancy_collocated(d, "co2", "rmse")
+    axs[0, i] = plotting.plot_monthly_average(d, "$CO_2$", axs[0, i])
+    axs[0, i].set_title(
+        site
+        + f", RMSE: {rmse_co2['rmse_co2'].values[0]:.2f},"
+        + f"\nBias: {rmse_co2['bias_co2'].values[0]:.2f}, "
+        + f"SD: {np.sqrt(rmse_co2['var_co2'].values[0]):.2f}",
+        fontsize="small",
+    )
+    axs[0, i].legend(fontsize="x-small", handlelength=0.4)
+
+for i, site in enumerate(sites_selected_ch4):
+    d = d_col_sel_ch4[d_col_sel_ch4.site_code == site].copy()
+    rmse_ch4 = compute_discrepancy_collocated(d, "ch4", "rmse")
+    axs[1, i] = plotting.plot_monthly_average(d, "$CH_4$", axs[1, i])
+    axs[1, i].set_title(
+        site
+        + f", RMSE: {rmse_ch4['rmse_ch4'].values[0]:.2f},"
+        + f"\nBias: {rmse_ch4['bias_ch4'].values[0]:.2f}, "
+        + f"SD: {np.sqrt(rmse_ch4['var_ch4'].values[0]):.2f}",
+        fontsize="small",
+    )
+    axs[1, i].legend(fontsize="x-small", handlelength=0.4)
+
+# %%
+plotting.plot_collocated_rmse(d_colloc_co2, d_colloc_ch4, "rmse")
