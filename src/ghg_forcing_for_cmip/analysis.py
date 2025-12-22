98fcdb7b4ec713c9853c8fa2becab9018f61cfc1
florence-bockting <florence.bockting@tu-dortmund.de>
2025-12-22 06:57:06 +0100

    add check_rhat

--- src/ghg_forcing_for_cmip/analysis.py
+++ src/ghg_forcing_for_cmip/analysis.py
@@ -4,6 +4,7 @@ analysis pipeline for incorporating EO into GB
 
 from typing import Any
 
+import arviz as az
 import bambi as bmb
 import pandas as pd
 
@@ -38,3 +39,30 @@ def fit_gb_from_eo(
     idata = model.fit(draws=1000, chains=4, random_seed=seed, target_accept=0.95)
 
     return model, idata
+
+
+def check_rhat(idata: Any, critical_rhat: float = 1.01) -> pd.DataFrame:
+    """
+    Inspect Rhat diagnostic as one diagnostic for MCMC
+
+    Parameters
+    ----------
+    idata :
+        fitted inference object
+
+    critical_rhat :
+        rhat threshold; values above this threshold
+        are considered as problematic
+
+    Returns
+    -------
+    :
+        number of variables with Rhat > critical_Rhat and
+        details about corresponding model parameters
+    """
+    res = az.summary(idata)
+    rhat_critical = res[res.r_hat > critical_rhat]
+
+    print(f"Number of Rhat > {critical_rhat}: {len(rhat_critical)}.")
+
+    return rhat_critical
