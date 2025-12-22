cdbbc4986c6020d932d297cd16c568986bbb3005
florence-bockting <florence.bockting@tu-dortmund.de>
2025-12-22 06:49:43 +0100

    add fit_gb_from_eo

--- src/ghg_forcing_for_cmip/analysis.py
+++ src/ghg_forcing_for_cmip/analysis.py
@@ -4,10 +4,13 @@ analysis pipeline for incorporating EO into GB
 
 from typing import Any
 
+import bambi as bmb
 import pandas as pd
 
 
-def fit_gb_from_eo(formula: str, df: pd.DataFrame, seed: int) -> Any:
+def fit_gb_from_eo(
+    formula: str, df: pd.DataFrame, seed: int, categorical: list[str]
+) -> Any:
     """
     Fit Bayesian regression model to predict GB from EO
 
@@ -19,12 +22,19 @@ def fit_gb_from_eo(formula: str, df: pd.DataFrame, seed: int) -> Any:
     df :
         data used for fitting
 
-    seed : int
+    seed :
         seed for reproducibility
+
+    categorical :
+        list with variables in formula considered
+        to be categorical
 
     Returns
     -------
     Any
         model and inference object
     """
-    pass
+    model = bmb.Model(formula=formula, data=df, categorical=categorical)
+    idata = model.fit(draws=1000, chains=4, random_seed=seed, target_accept=0.95)
+
+    return model, idata
